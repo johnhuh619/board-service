74aecf10416f4109cc46ffb88df197bb4e6799fb
허재원 <jaewonheo666@gmail.com>
2025-02-22 16:38:55 +0900

    [Feat] PostController 구현

---
+++ src/main/java/com/example/board_service/controller/PostController.java
@@ -0,0 +1,51 @@
+package com.example.board_service.controller;
+
+import com.example.board_service.domain.Post;
+import com.example.board_service.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private final PostService postService;
+
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post, @RequestParam Long authorId) {
+        Post createdPost = postService.createPost(post, authorId);
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<Post> getPost(@PathVariable Long postId) {
+        Post post = postService.getPostById(postId);
+        return ResponseEntity.ok(post);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts() {
+        List<Post> posts = postService.getPosts();
+        return ResponseEntity.ok(posts);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long postId, @RequestBody Post updatedPost, @RequestParam Long authorId) {
+        Post post = postService.updatePost(postId, updatedPost, authorId);
+        return ResponseEntity.ok(post);
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long postId, @PathVariable Long authorId) {
+        postService.deletePost(postId, authorId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
