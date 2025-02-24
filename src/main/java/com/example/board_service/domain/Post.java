38083ed6470f0410416a0ca229977d3a2ab32016
허재원 <jaewonheo666@gmail.com>
2025-02-24 14:33:04 +0900

    [Fix] Post 생성자 오류 수정

--- src/main/java/com/example/board_service/domain/Post.java
+++ src/main/java/com/example/board_service/domain/Post.java
@@ -1,9 +1,7 @@
 package com.example.board_service.domain;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +11,8 @@ import java.util.List;
 @Entity
 @Table(name = "posts")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
 
     @Id
