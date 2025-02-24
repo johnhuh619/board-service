beb8583a7be0387fdc498dee3d6cac294dbf252a
허재원 <jaewonheo666@gmail.com>
2025-02-24 15:20:39 +0900

    [Feat] JWT 토큰을 사용한 로그인을 위해 Jwt 유틸리티 클래스 생성

---
+++ src/main/java/com/example/board_service/util/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.example.board_service.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.PublicKey;
+import java.util.Date;
+
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    //토큰을 생성한다. username => subject 로 유효기간 설정
+    public String generateToken(String username) {
+        long expirationMillis = 1000 * 60 * 60; // 1h
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    // 토큰에서 (String) username 을 추출한다.
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    // 토큰 검증: 만료 여부 & username 일치 여부
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    // 토큰 만료 여부를 확인
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    // JWT Claims 추출
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
