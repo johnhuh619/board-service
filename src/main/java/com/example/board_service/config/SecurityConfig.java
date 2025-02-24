2c5eb183858c879427b3907d8d39b01463e64ee6
허재원 <jaewonheo666@gmail.com>
2025-02-24 16:04:47 +0900

    [Fix] security config에 jwt 필터 추가.

--- src/main/java/com/example/board_service/config/SecurityConfig.java
+++ src/main/java/com/example/board_service/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.example.board_service.config;
 
+import com.example.board_service.util.JwtAuthenticationFilter;
+import com.example.board_service.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
@@ -25,12 +35,13 @@ public class SecurityConfig {
                 )
                 // H2 콘솔 및 정적 리소스 접근 허용
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/h2-console/**", "/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 // 개발용 기본 로그인 폼 사용
-                .formLogin(Customizer.withDefaults());
-
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(AbstractHttpConfigurer::disable);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
--- src/main/java/com/example/board_service/util/JwtUtil.java
+++ src/main/java/com/example/board_service/util/JwtUtil.java
@@ -4,10 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-import java.security.PublicKey;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
